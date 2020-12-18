50a66e15f363cf27948ba082c36046ffd1a2d1cb
Travis <travisfinch01@gmail.com>
2020-12-18 14:36:54 -0500

    Base ERC20

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.6.2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -5,102 +7,54 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import "../Interfaces/IToken.sol";
+import "./Interfaces/IToken.sol";
 
-contract Token is ERC20("Block Duelers", "DUEL"), Ownable, ReentrancyGuard, AccessControl{
-  using SafeMath for uint256;
+contract Token is ERC20("Block Duelers", "BDT"), Ownable, AccessControl{
+    using SafeMath for uint256;
 
-  uint public  INITIAL_SUPPLY = 12000e18;
+    bytes32 private constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 private constant SETTER_ROLE = keccak256("SETTER_ROLE");
 
-  address public duelersAddress;
-  address public NFTAddress;
+    uint public _totalSupply = 50000e18; //50,000
 
-  constructor() public {
-        duelersAddress = msg.sender;
-        _mint(msg.sender, INITIAL_SUPPLY);
-  }
+    //event Burn(address indexed from, uint256 value);
+    //event Transfer(address indexed from, address indexed to, uint tokens);
+    //event Mint(address indexed _address, uint _reward);
 
-  function setNFTAddress(address _address) public onlyOwner {
-    NFTAddress = _address;
-  }
+    mapping(address => uint256) private balances;
 
-  mapping(address => uint256) private duelersBalance;
-  mapping(address => uint256) public lastUpdateTime;
-  mapping(address => uint256) public points;
-
-  event Staked(address indexed user, uint256 amount);
-  event Withdrawn(address indexed user, uint256 amount);
-
-  modifier updateReward(address account) {
-    if (account != address(0)) {
-      points[account] = earned(account);
-      lastUpdateTime[account] = block.timestamp;
+    modifier onlyMinter() {
+        require(hasRole(MINTER_ROLE, _msgSender()), "Caller is not a minter");
+        _;
     }
-    _;
-  }
 
-  function _balanceOf(address account) public view returns (uint256) {
-    return duelersBalance[account];
-  }
+    modifier onlySetter() {
+        require(hasRole(SETTER_ROLE, _msgSender()), "Caller is not a setter");
+        _;
+    }
 
-  /*
-  The block.timestamp environment variable is used to determine a control flow decision. 
-  Note that the values of variables like coinbase, gaslimit, block number and timestamp are predictable and can be manipulated by a malicious miner. 
-  Also keep in mind that attackers know hashes of earlier blocks. 
-  Don't use any of those environment variables as sources of randomness and be aware that use of these variables introduces a certain level of trust into miners.
-  https://swcregistry.io/docs/SWC-116
-  
-  AUDITOR NOTE: It appers that intent of this function is to add incremental rewards over time.
-    Thus this vulnerability is not relly applicable to this function.
-  */
-  function earned(address account) public view returns (uint256) {
-    uint256 blockTime = block.timestamp;
-    return points[account].add(blockTime.sub(lastUpdateTime[account]).mul(1e18).div(5760).mul(balanceOf(account).div(1e18)));
-  }
-
-  /*
-  An external message call to an address specified by the caller is executed.
-  Note that the callee account might contain arbitrary code and could re-enter any function within this contract. 
-  Reentering the contract in an intermediate state may lead to unexpected behaviour. 
-  Make sure that no state modifications are executed after this call and/or reentrancy guards are in place.
-  https://swcregistry.io/docs/SWC-107
-  
-  AUDITOR NOTE: Fix implmented, and vulnerable code commented out.
-  */
-  function stake(uint256 amount) public updateReward(_msgSender()) nonReentrant {
-    require(amount.add(balanceOf(_msgSender())) >= 2e18, "Cannot stake less than 2 DUELERS");
-    require(amount.add(balanceOf(_msgSender())) <= 10e18, "Cannot stake more than 10 DUELERS");
-    duelersBalance[_msgSender()] = duelersBalance[_msgSender()].add(amount);
-    IERC20(duelersAddress).transferFrom(_msgSender(), address(this), amount);
-    emit Staked(_msgSender(), amount);
-  }
-
-  function withdraw(uint256 amount) public updateReward(_msgSender()) nonReentrant {
-    require(amount > 0, "Cannot withdraw 0");
-    require(amount <= balanceOf(_msgSender()), "Cannot withdraw more than balance");
-    IERC20(duelersAddress).transfer(_msgSender(), amount);
-    duelersBalance[_msgSender()] = duelersBalance[_msgSender()].sub(amount);
-    emit Withdrawn(_msgSender(), amount);
-  }
-
-  function exit() external {
-    withdraw(balanceOf(_msgSender()));
-  }
-
-  mapping(uint256 => uint256) public redeemCost;
-  
-  function setRedeemCost(uint256 _id, uint256 _cost) public onlyOwner {
-    redeemCost[_id] = _cost;
-  }
-
+    constructor(
+        //address _setter
+    ) public {
+        _setupRole(MINTER_ROLE, msg.sender);
+        _setupRole(SETTER_ROLE, msg.sender);
     
-  function redeem(uint256 _id) public updateReward(_msgSender()) nonReentrant {
-    uint256 price = redeemCost[_id];
-    require(price > 0, "Card not found");
-    require(points[_msgSender()] >= price, "Not enough points to redeem");
-    IToken(NFTAddress).mint(_msgSender(), _id, 1);
-    points[_msgSender()] = points[_msgSender()].sub(price);
-  }
-}
+        _mint(msg.sender, _totalSupply);
+    }
 
+    function getMinterRole() external pure returns (bytes32) {
+        return MINTER_ROLE;
+    }
+
+    function getSetterRole() external pure returns (bytes32) {
+        return SETTER_ROLE;
+    }
+
+    function mint(address to, uint256 amount) external onlyMinter {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyMinter {
+        _burn(from, amount);
+    }
+}

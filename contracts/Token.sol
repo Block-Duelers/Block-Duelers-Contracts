91bee8eee8d41c2496f7053aa55939e19b06941d
TFinch <travisfinch01@gmail.com>
2020-12-19 18:02:31 -0500

    Openzeppelin Base

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -7,22 +7,13 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
-import "./Interfaces/IToken.sol";
 
 contract Token is ERC20("Block Duelers", "BDT"), Ownable, AccessControl {
     using SafeMath for uint256;
 
-    bytes32 private constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 private constant SETTER_ROLE = keccak256("SETTER_ROLE");
 
     uint256 public _totalSupply = 50000e18; //50,000
-
-    mapping(address => uint256) private balances;
-
-    modifier onlyMinter() {
-        require(hasRole(MINTER_ROLE, _msgSender()), "Caller is not a minter");
-        _;
-    }
 
     modifier onlySetter() {
         require(hasRole(SETTER_ROLE, _msgSender()), "Caller is not a setter");
@@ -30,25 +21,20 @@ contract Token is ERC20("Block Duelers", "BDT"), Ownable, AccessControl {
     }
 
     constructor() public {
-        _setupRole(MINTER_ROLE, msg.sender);
         _setupRole(SETTER_ROLE, msg.sender);
 
         _mint(msg.sender, _totalSupply);
-    }
-
-    function getMinterRole() external pure returns (bytes32) {
-        return MINTER_ROLE;
     }
 
     function getSetterRole() external pure returns (bytes32) {
         return SETTER_ROLE;
     }
 
-    function mint(address to, uint256 amount) external onlyMinter {
-        _mint(to, amount);
+    function getBalance(address account) public view {
+        balanceOf(account);
     }
 
-    function burn(address from, uint256 amount) external onlyMinter {
-        _burn(from, amount);
+    function transferTo(address to, uint256 amount) public {
+        transfer(to, amount);
     }
 }
